dcb93337815d70e66c274ae1ef72afaeb6e30323
fralonra <fralonra@aliyun.com>
2019-02-20 20:21:20 +0800

    v1.0.2: use standard to format

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const fp = require('fastify-plugin');
-const style = require('ansi-styles');
-const momment = require('moment');
+const fp = require('fastify-plugin')
+const style = require('ansi-styles')
+const momment = require('moment')
 
 const defaultOptions = {
   allInOne: false,
@@ -10,53 +10,52 @@ const defaultOptions = {
   info: '#ffffff',
   warn: '#ffa500',
   error: '#dc143c'
-};
+}
 
 function plugin (fastify, opts, next) {
-
-  const options = Object.assign(defaultOptions, opts);
+  const options = Object.assign(defaultOptions, opts)
 
   function info () {
-    if (options.level > 1) return;
-    log('info', arguments);
+    if (options.level > 1) return
+    log('info', arguments)
   }
 
   function warn () {
-    if (options.level > 2) return;
-    log('warn', arguments);
+    if (options.level > 2) return
+    log('warn', arguments)
   }
 
   function error () {
-    if (options.level > 3) return;
-    log('error', arguments);
+    if (options.level > 3) return
+    log('error', arguments)
   }
 
   function log (type, text) {
-    const time = options.time ? momment().format(options.timeFormat) + ' ': '';
-    console.log(style.color.ansi16m.hex(options[type]) + time + concat(text), style.color.close);
+    const time = options.time ? momment().format(options.timeFormat) + ' ' : ''
+    console.log(style.color.ansi16m.hex(options[type]) + time + concat(text), style.color.close)
   }
 
-  function concat(args) {
-    return Array.from(args).join(' ');
+  function concat (args) {
+    return Array.from(args).join(' ')
   }
 
   const logger = {
     info,
     warn,
     error
-  };
-
-  if (options.allInOne) {
-    fastify.decorate('logger', logger);
-  } else {
-    Object.keys(logger).forEach(k => {
-      fastify.decorate(k, logger[k]);
-    });
   }
 
-  next();
+  if (options.allInOne) {
+    fastify.decorate('logger', logger)
+  } else {
+    Object.keys(logger).forEach(k => {
+      fastify.decorate(k, logger[k])
+    })
+  }
+
+  next()
 }
 
 module.exports = fp(plugin, {
   name: 'fastify-logger'
-});
+})
